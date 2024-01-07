df3bec8ccff3ab4adc3d87a3e13cfeb3a0d03d7d
hutanmihai <hutanmihai29@gmail.com>
2024-01-07 18:03:21 +0200

    continue

--- src/pre_training.py
+++ src/pre_training.py
@@ -1,6 +1,12 @@
 from src.utils.collapse import collapse
 from src.utils.generate_positives_negatives import extract_train_and_validation_patches
 from src.utils.visualize import visualize_images_with_boxes, visualize_images_with_boxes_and_detections
+from src.constants import (
+    VALIDATION_IMAGES_PATH,
+    VALIDATION_ANNOTATIONS_PATH,
+    COLLAPSED_IMAGES_PATH,
+    COLLAPSED_ANNOTATIONS_PATH,
+)
 
 if __name__ == "__main__":
     # Collapse the images and annotations
